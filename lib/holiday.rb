1f06024abfc11d7def03d511317107cb6cce3671
Abel Kalum <chumbabel@gmail.com>
2020-01-28 16:27:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -4,49 +4,6 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_hash, "Balloons")
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations << "Balloons"
-  end
-end
-
-
-def add_supply_to_winter_holidays(holiday_hash, item)
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations.push(item)
-  end
-end
-
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
-end
-
-end
-
-def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].values.flatten
-
-end
-
-def all_supplies_in_holidays(holiday_hash)
-   holiday_hash.each do | season, holiday|
-     puts "#{season.capitalize}:"
-     holiday.each do |holiday, item|
-       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-     end
-  end 
-end
-
-def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
-    end 
-  end.flatten.compact
-end
-
-
 
 
 

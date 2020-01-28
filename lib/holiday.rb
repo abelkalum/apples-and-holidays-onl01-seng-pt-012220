e6b9d18da6d082821187c67d404dce57c1dfe363
Abel Kalum <chumbabel@gmail.com>
2020-01-28 16:27:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -4,7 +4,11 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-
+def add_supply_to_winter_holidays(holiday_hash, "Balloons")
+ holiday_hash[:winter].each do |holiday, decorations|
+   decorations << "Balloons"
+  end
+end
 
 
 
